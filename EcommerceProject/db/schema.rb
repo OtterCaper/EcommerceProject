c0b807a428f3f9a457d1c342124e1aeec4709fb5
OtterCaper <ian.de.groot.j@gmail.com>
2020-11-13 17:07:56 -0600

    add Categories migration

--- EcommerceProject/db/schema.rb
+++ EcommerceProject/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_13_174250) do
+ActiveRecord::Schema.define(version: 2020_11_13_175158) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -57,6 +57,12 @@ ActiveRecord::Schema.define(version: 2020_11_13_174250) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_admin_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
