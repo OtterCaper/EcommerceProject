fee3d5786a4ed0ec0a9de941d8951e9ccc6caa60
OtterCaper <ian.de.groot.j@gmail.com>
2020-11-15 19:24:57 -0600

    2.2 Navigate through the available products by category

--- EcommerceProject/config/routes.rb
+++ EcommerceProject/config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
+  # get 'categories/index'
+  # get 'categories/show'
   # get 'products/index'
   # get 'products/show'
   get 'about/show'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   resources :products, only: %i[index show]
+  resources :categories, only: %i[index show]
   # resources :products, only: :show do
   #   collection do
   #     get :search
