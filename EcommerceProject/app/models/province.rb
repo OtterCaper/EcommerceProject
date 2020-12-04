47960e7475c89e24484ff3fc522581934995c560
OtterCaper <ian.de.groot.j@gmail.com>
2020-12-04 16:52:04 -0600

    3.1.3 Fix issue with updating province and add link to updaing user address in invoice page

--- EcommerceProject/app/models/province.rb
+++ EcommerceProject/app/models/province.rb
@@ -1,3 +1,2 @@
 class Province < ApplicationRecord
-  has_many :users
 end
--- EcommerceProject/app/models/user.rb
+++ EcommerceProject/app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ApplicationRecord
-  belongs_to :province
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
